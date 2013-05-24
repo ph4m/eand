8a9e2533f3aa9b88abddc3cc34a77003fbeaf38b
Tu-Hoa Pham <pham.main@gmail.com>
2013-05-24 14:07:18 +0300

    Fixed various minor bugs with dimensionality

--- eand/mddig/demo/multidiff_demo.py
+++ eand/mddig/demo/multidiff_demo.py
@@ -23,7 +23,7 @@ from eand.mddig.multidiff import MultiDiff
 
 print 'Initializing estimation parameters...'
 
-Ns = 500
+Ns = 4000
 t1Min = -1.0
 t1Max = 1.0
 t2Min = -1.0
@@ -36,14 +36,15 @@ for i in range(Ns):
     t2[i] = t2Min+(t2Max-t2Min)*np.random.random()
 
 n1 = 1
-alpha1 = 1
-beta1 = 1
+alpha1 = 3
+beta1 = 3
 n2 = 0
-alpha2 = 0
-beta2 = 0
+alpha2 = 2
+beta2 = 2
 T1 = 0.25
 T2 = 0.25
 
+'''
 # 1D case
 paramVec = [[n1,alpha1,beta1,T1]]
 tVec = [t1]
@@ -51,9 +52,11 @@ tVec = [t1]
 # 2D case
 paramVec = [[n1,alpha1,beta1,T1],[n2,alpha2,beta2,T2]]
 tVec = [t1,t2]
-'''
 
-signal = [cos(2*sum([t[i] for t in tVec])) for i in range(len(tVec[0]))]
+nDim = len(tVec)
+nSamples = len(tVec[0])
+
+signal = [cos(2*sum([t[i] for t in tVec])) for i in range(nSamples)]
 
 print 'Building differentiator...'
 multiDiff = MultiDiff(paramVec,tVec)
@@ -66,9 +69,10 @@ print 'Commencing differentiation...'
 
 print 'Calculating reference derivative...'
 tSlice,dSlice = multiDiff.plotSlice(tPostVec,dPost,1,0.,0.1,0)
-dRef = []
-for i in tSlice:
-    dRef.append(-2*sin(2*i))
+if sum([paramVec[i][0] for i in range(nDim)]) == 1:
+    dRef = [-2*sin(2*i) for i in tSlice]
+elif sum([paramVec[i][0] for i in range(nDim)]) == 2:
+    dRef = [-4*cos(2*i) for i in tSlice]
 plt.plot(tSlice,dRef,'r.')
 
 print 'Plotting derivative estimate...'
--- eand/mddig/demo/multidiffrep_demo.py
+++ eand/mddig/demo/multidiffrep_demo.py
@@ -23,7 +23,7 @@ from eand.mddig.multidiffrep import MultiDiffRep
 
 print 'Initializing estimation parameters...'
 
-Ns = 1000
+Ns = 4000
 t1Min = -1.0
 t1Max = 1.0
 t2Min = -1.0
@@ -36,8 +36,8 @@ for i in range(Ns):
     t2[i] = t2Min+(t2Max-t2Min)*np.random.random()
 
 n1 = 1
-alpha1 = 1
-beta1 = 1
+alpha1 = 3
+beta1 = 3
 n2 = 0
 alpha2 = 0
 beta2 = 0
@@ -57,37 +57,39 @@ paramVec2 = [[n2,alpha2,beta2,T2],[n1,alpha1,beta1,T1]] # then along the second 
 paramVecSeq = [paramVec1,paramVec2]
 tVec = [t1,t2]
 
+nDim = len(tVec)
+nSamples = len(tVec[0])
+nDiffStep = len(paramVecSeq)
 
-signal = [cos(2*sum([t[i] for t in tVec])) for i in range(len(tVec[0]))]
+signal = [cos(2*sum([t[i] for t in tVec])) for i in range(nSamples)]
 
 print 'Building successive differentiators...'
 multiDiffRep = MultiDiffRep(paramVecSeq,tVec)
 
-print 'Plotting successive partitions...'
-for step in range(len(paramVecSeq)):
-    multiDiffRep.differentiators[step].plotPartition(0)
-
 print 'Commencing differentiation...'
 (tPostVecSeq,dPostSeq) = multiDiffRep.differentiate(signal)
 
-print 'Calculating reference derivative...'
-for step in range(len(paramVecSeq)):
-    tSlice,dSlice = multiDiffRep.differentiators[step].plotSlice(tPostVecSeq[step],dPostSeq[step],1,0.,0.1,0)
-    dRef = []
-    if step == 0:
-        for i in tSlice:
-            dRef.append(-2*sin(2*i))
-    elif step == 1:
-        for i in tSlice:
-            dRef.append(-4*cos(2*i))
-    plt.plot(tSlice,dRef,'r.')
+print 'Plotting initial signal...'
+multiDiffRep.differentiators[0].plotScatter(tVec, signal,0)
 
+print 'Plotting successive partitions...'
+for step in range(nDiffStep):
+    multiDiffRep.differentiators[step].plotPartition(0)
+    
 print 'Plotting derivative estimate...'
 for step in range(len(paramVecSeq)):
     multiDiffRep.differentiators[step].plotScatter(tPostVecSeq[step], dPostSeq[step],0)
     multiDiffRep.differentiators[step].plotSurface(tPostVecSeq[step], dPostSeq[step],0)
 
+print 'Plotting estimate slice and reference derivative...'
+for step in range(nDiffStep):
+    tSlice,dSlice = multiDiffRep.differentiators[step].plotSlice(tPostVecSeq[step],dPostSeq[step],1,0.,0.1,0)
+    if sum([sum([paramVecSeq[i][j][0] for j in range(nDim)]) for i in range(step+1)]) == 1:
+        dRef = [-2*sin(2*i) for i in tSlice]
+    elif sum([sum([paramVecSeq[i][j][0] for j in range(nDim)]) for i in range(step+1)]) == 2:
+        dRef = [-4*cos(2*i) for i in tSlice]
+    plt.plot(tSlice,dRef,'r.')
+
 plt.show()
 
 print 'SUCCESS!'
-
